ff02cc71592b68727542363477878e13dfa52165
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2022-08-08 01:00:37 +0200

    fetch direct dependencies of packages along with packages

--- app/Rift/Commands/Impl/FetchPackage.hs
+++ app/Rift/Commands/Impl/FetchPackage.hs
@@ -4,6 +4,7 @@
 
 module Rift.Commands.Impl.FetchPackage where
 
+import Control.Monad (forM_)
 import Control.Monad.Catch (MonadMask)
 import Control.Monad.Extra (unlessM)
 import Control.Monad.IO.Class (MonadIO, liftIO)
@@ -17,10 +18,10 @@ import Dhall (auto, inputFile)
 import Network.HTTP.Req (MonadHttp)
 import Rift.Commands.Impl.Utils.Download (downloadAndExtract)
 import Rift.Config.PackageSet (LTSVersion (..), Package (..), Snapshot (..), readLTSVersion)
-import Rift.Config.Project (packageSourceToDependency)
+import Rift.Config.Project (ComponentType (..), ProjectType (..), VersionRange (..), packageSourceToDependency)
 import Rift.Environment (Environment (..))
 import qualified Rift.Logger as Logger
-import System.Directory (doesFileExist)
+import System.Directory (doesDirectoryExist, doesFileExist)
 import System.Exit (exitFailure)
 import System.FilePath ((<.>), (</>))
 
@@ -46,9 +47,33 @@ fetchPackageCommand name versionConstraint ltsName env = do
       liftIO exitFailure
     p : ps -> do
       -- TODO: take @versionConstraint@ in account
-      let dir = ltsDir </> "sources" </> Text.unpack name <.> Text.unpack (version p)
+      go ltsDir p
+  where
+    go :: (MonadIO m, MonadHttp m, MonadMask m) => FilePath -> Package -> m ()
+    go ltsDir (Pkg name version src component _ _) = do
+      let dir = ltsDir </> "sources" </> Text.unpack name <.> Text.unpack version
 
-      -- TODO: do transitively: also download all dependencies after
-      (_, project) <- downloadAndExtract (\_ _ _ -> dir) (packageSourceToDependency $ src p) env
+      unlessM (liftIO $ doesDirectoryExist dir) do
+        -- TODO: do transitively: also download all dependencies after
+        (_, ProjectType components _ extra) <- downloadAndExtract (\_ _ _ -> dir) (packageSourceToDependency src) env
+        deps <- case (component, components) of
+          (Nothing, _ : _ : _) -> do
+            Logger.error "Cannot choose a single component from the package."
+            liftIO exitFailure
+          (Nothing, [c]) -> do undefined
+          (_, []) -> do
+            Logger.error "No component found in package."
+            liftIO exitFailure
+          (Just comp, cs) ->
+            case filter (\(ComponentType c _ _ _ _ _) -> c == comp) cs of
+              [] -> do
+                Logger.error $ "Component '" <> comp <> "' not found in package."
+                liftIO exitFailure
+              _ : _ : _ -> do
+                Logger.error $ "Multiple components named '" <> comp <> "' found in the package."
+                liftIO exitFailure
+              [ComponentType _ _ deps _ _ _] -> pure deps
 
-      pure ()
+        forM_ deps \(Version name range) -> fetchPackageCommand name (Just range) ltsName env
+
+        pure ()
